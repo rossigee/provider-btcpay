757b5d40584ac12fe8a2c2475c2d78706457e835
Ross Golder <ross@golder.org>
2026-09-10 19:45:48 +0700

    fix: remove broken v1beta1 controllers
    Remove incorrectly generated v1beta1 controllers that had build errors.
    Keep v1beta1 API types but remove controllers until properly implemented.

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -19,10 +19,8 @@ package controller
 import (
 	"github.com/crossplane/crossplane-runtime/v2/pkg/controller"
 	"github.com/rossigee/provider-btcpay/internal/controller/invoice"
-	"github.com/rossigee/provider-btcpay/internal/controller/invoicev1beta1"
 	"github.com/rossigee/provider-btcpay/internal/controller/providerconfig"
 	"github.com/rossigee/provider-btcpay/internal/controller/store"
-	"github.com/rossigee/provider-btcpay/internal/controller/storev1beta1"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
@@ -34,9 +32,7 @@ func Setup(mgr ctrl.Manager, o controller.Options) error {
 	}
 	for _, setup := range []func(ctrl.Manager, controller.Options) error{
 		store.Setup,
-		storev1beta1.Setup,
 		invoice.Setup,
-		invoicev1beta1.Setup,
 	} {
 		if err := setup(mgr, o); err != nil {
 			return err
